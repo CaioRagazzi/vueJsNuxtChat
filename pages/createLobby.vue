2e63599bf923202cedc906974cd92bfedaac668a
Caio <ca.ragazzi@gmail.com>
2020-06-02 20:59:39 -0300

    first commit

---
+++ pages/createLobby.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <b-form v-if="show" @submit.prevent="onSubmit" @reset="onReset">
+      <b-form-group
+        id="input-group-1"
+        label="Lobby Name:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          required
+          placeholder="Enter lobby name"
+        />
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name" />
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-button @click="back()">Back</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: true,
+      form: { email: '', name: '' }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.form.email, this.form.name)
+    },
+    onReset () {
+      console.log('reset')
+    },
+    back () {
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <div>
+      <b-button variant="outline-primary">
+        <nuxt-link to="/createLobby">
+          Create Lobby
+        </nuxt-link>
+      </b-button>
+      <b-button class="ml-2" variant="outline-primary" @click="show">
+        Access Lobby
+      </b-button>
+    </div>
+    <transition name="slide-fade">
+      <b-form-input v-if="showInput" v-model="text" class="mt-3" placeholder="Enter Lobby ID" />
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      text: '',
+      showInput: false
+    }
+  },
+  methods: {
+    show () {
+      this.showInput = true
+    }
+  }
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  max-width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
